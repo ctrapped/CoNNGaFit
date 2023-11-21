1fde599e2df691ba85d169f2d7de2b2b3f931a83
ctrapped <ctrapped@gmail.com>
2023-11-21 11:48:28 -0800

    Cleaned up and commented for ease of use.

--- CoNNGaFit_UseModel.py
+++ CoNNGaFit_UseModel.py
@@ -1,20 +1,13 @@
 import torch
-from torch import nn
+#from torch import nn
 from torch.utils.data import DataLoader
-from torchvision import datasets
+#from torchvision import datasets
 from torchvision.transforms import ToTensor, Lambda, Normalize, Compose
-import sys
 from CoNNGaFit_InferenceDatasets import CoNNGaFitImageInferenceDataset
-
-#try:
-#    networkType = sys.argv[1]
-#except:
-#    networkType = "shallow"
-
 
 
 import numpy as np
-from sklearn.preprocessing import StandardScaler    
+#from sklearn.preprocessing import StandardScaler    
 from matplotlib import pyplot as plt
 from matplotlib.colors import LogNorm
 import scipy.stats as stats
@@ -27,149 +20,97 @@ print(f'Using {device} device')
 
 CUDA_LAUNCH_BLOCKING=1
 
-#training_data = CoNNGaFitDatasets.Fire2_cr_datasets( #Probably need to update completely with your own data loading
-#    root="data",
-#    train=True,
-#    download=True,
-#    transform=ToTensor()
-#)
-
-
 Sim2PhysicalUnits_MassFlux = (2/np.pi) * 1/(3.086*np.power(10.,16.)) * (3.154*np.power(10.,7.)) * np.power(10,10) #1/pixel_res * kpc2km * s2yr * unit mass to solar masses
-saveItr=0
+
+####Functions to calculate inferences on HI spectral datacubes from a previously trained CoNNGaFit network. Generates a projection map and a radial plot for the fitted parameter of interest. 
+####For use, you need the trained network and the images to run the inferences on with a corresponding .csv file listing their location. 
+####Call RunInferences, providing the network type you wish to use, the output directory, and the .csv file containing the input images. See InferenceTemplate.py for an example
+####
+####Written By Cameron Trapp (ctrapped@gmail.com)
+####Updated 11/21/2023
+
+
+def RunInferences(networkType,imageOutput_prefix,imageList,params=None):
 
 def LoadNames(inputDir):
+    ####Get a distinct name for each image to run inferences on
     Nlines=0
     fid = open(inputDir,'r')
-    for line in fid:
-        Nlines+=1
-    fid.close()
-    fid = open(inputDir,'r')
-    names = np.zeros((Nlines)).astype('str')
-    i=0
+    names = np.zeros((0)).astype('str')
     for line in fid:
         x=line.split("\\")
         Nsplit = np.size(x)
-        y=(x[Nsplit-1]).split('.')
-        names[i] = y[0]
-        i+=1
-        #print("y=",y)
-        #print("y0=",y[0])
+        y=(x[Nsplit-1]).split('.') #get filename in directory
+        names=np.append(names,y[0]) #ignore filetype
         
     fid.close()
-        
+    
     return names
 
     
-def MakeInitialDiagnosticImage(dataloader):
-    size = len(dataloader.dataset)
-    num_batches=len(dataloader)
-
-    actuals = np.array([])
-    
-    print("Making initial diagnostic image...")
-    
-    with torch.no_grad():
-        for X, y in dataloader:
-            y=y.to(device)
-            actuals = np.append(actuals,y.cpu().detach().numpy())
-            vmax = np.max(np.abs(actuals))
-            vmin = -vmax
-            MakeImage(actuals,imageOutput_input+str(saveItr)+"_input_"+str(epoch)+".png",vmin,vmax,tt)
 
 
-def CompileImages(data,output,tag):
-    i=NpixelNeighbors-1
-    j = NpixelNeighbors-1
-    image = np.zeros((Npixels,Npixels))
-    Nsnap=0
-    k=0
-    
-    print("Trying to compile image...")
-    
-    for idx in range(0,np.size(data)):
-        flag = data[idx]
-        if k<Nspec:
-            image[j,i]+=flag
-        else:
-            k=-1
-            j+=1
-            if (j>Npixels-NpixelNeighbors):
-                j=NpixelNeighbors-1
-                i+=1
-                if i>Npixels-NpixelNeighbors:
-                    fig = plt.figure()
-                    vmin = np.min(image)
-                    vmax = np.max(image)
-                    if -vmin>vmax:
-                        vmax = -vmin
-                    else:
-                        vmin = -vmax
-                    plt.imshow(image)
-                    plt.title("Snapshot "+str(Nsnap)+": "+tag)
-                    plt.colorbar()
-                    plt.savefig(output+"_"+str(Nsnap)+"_"+tag+".png")
-                    Nsnap+=1
-                    image = 0*image
-                    i = NpixelNeighbors-1
-        
-        k+=1
-        
-    if Nsnap==0:
-                    fig = plt.figure()
-
-                    plt.imshow(image)
-                    plt.colorbar()
-                    plt.title("Snapshot "+str(Nsnap)+": "+tag)
-                    plt.savefig(output+"_"+str(Nsnap)+"_"+tag+".png")
-
-
-
-def MakeImage(data,output,vmin,vmax,targetShape=[20,20]):
+def MakeImage(data,output,vmin=None,vmax=None,targetShape=None):
     image = np.zeros((np.shape(data)[1]))
     image[:] = data[:]
-    npix = int(np.round(np.sqrt(np.size(image))))
-    image = np.reshape(image,targetShape)
-    #image -= np.min(image)
-    #image=np.sum(image,2)
+    
+    if targetShape is None:
+        npix = int(np.round(np.sqrt(np.size(image))))
+        targetShape=[npix,npix]
+        
+    image = np.reshape(image,targetShape) #make into 2d image
+
     plt.figure()
-    vmax = np.max( [-np.min(image) , np.max(image)])
-    vmin=-vmax
-    saturationFactor=1
-    plt.imshow(image*Sim2PhysicalUnits_MassFlux,cmap='seismic',vmin=vmin*Sim2PhysicalUnits_MassFlux*saturationFactor,vmax=vmax*Sim2PhysicalUnits_MassFlux*saturationFactor)
+    
+    if vmax is None or vmin is None:
+        vmax = np.max( [-np.min(image) , np.max(image)]) # default to symmetric limits
+        vmin=-vmax
+    
+    #Plot in units of Solar Masses per year
+    plt.imshow(image*Sim2PhysicalUnits_MassFlux,cmap='seismic',vmin=vmin*Sim2PhysicalUnits_MassFlux,vmax=vmax*Sim2PhysicalUnits_MassFlux)
     plt.colorbar(label = 'Radial Mass Flux [M$_{\odot}$ yr$^{-1}$]');
-    #plt.title("Radial Mass Flux")
     plt.savefig(output+"projectionMap.png")
     plt.close()
     
+    #Save hdf5 for generating figures
     hf.File(output+"projectionMap.hdf5")
     hf.create_dataset('image',data=image)
     hf.close()
 
-def CreateBasicPlots(data,output):
+def CreateBasicPlots(data,output,targetShape=None,nBins=20):
     image = np.zeros((np.shape(data)[1]))
     image[:] = data[:]
-    npix = int(np.round(np.sqrt(np.size(image))))
-    image = np.reshape(image,[20,20])
-    centerIndex = [9,9]
+    if targetShape is None:
+        npix = int(np.round(np.sqrt(np.size(image))))
+        targetShape=[npix,npix]
+    
+    npix=targetShape[0]
+    image = np.reshape(image,targetShape) #make into 2d image
+    
+    ## Calculate galactocentric radius of each pixel
+    centerIndex = [npix/2-1,npix/2-1]
     indices = np.indices(np.shape(image))
     rmag = 2*np.sqrt( np.power(indices[0,:,:]-centerIndex[0] , 2) + np.power(indices[1,:,:] -centerIndex[1], 2) )
     
-    nBins = 20
+    ##Calculate total mass flux as function of radius
     binRange=[0,np.max(rmag)]
-    print("nBins=",nBins)
     binned_data,binedge,binnum = stats.binned_statistic_dd(rmag.flatten(),image.flatten(),"sum",nBins,range=[binRange])
     
-    plt.figure()
+    #Default to symmetric y-limits
     vmax = np.max( [-np.min(binned_data* Sim2PhysicalUnits_MassFlux) , np.max(binned_data* Sim2PhysicalUnits_MassFlux)])*1.05
     vmin=-vmax
-    plt.plot(np.linspace(0,np.max(rmag),nBins) , binned_data* Sim2PhysicalUnits_MassFlux , 'k', lw = 2.5)
-    plt.plot(np.linspace(0,np.max(rmag),nBins) , binned_data*0 , 'k--', lw = 1.5)
+    
+    #Plot in units of Solar Masses per year
+    plt.figure()
+    plt.plot(np.linspace(0,np.max(rmag),nBins) , binned_data* Sim2PhysicalUnits_MassFlux , 'k', lw = 2.5) #plot azimuthal average
+    plt.plot(np.linspace(0,np.max(rmag),nBins) , binned_data*0 , 'k--', lw = 1.5) #plot zero line
     plt.xlabel('Radius [kpc]')
     plt.ylabel('Radial Mass Flux [M$_{\odot}$ yr$^{-1}$]')
     plt.ylim([vmin,vmax])
     plt.savefig(output+"radialPlot.png")
     plt.close()
+    
+    #Save hdf5 for generating figures
     hf.File(output+"radialPlot.hdf5")
     hf.create_dataset('binned_data',data=binned_data)
     hf.create_dataset('rPlot',data=np.linspace(0,np.max(rmag),nBins))
@@ -178,49 +119,43 @@ def CreateBasicPlots(data,output):
    
     
     
-def RunInferences(networkType,modelOutputPath, dirName,imageOutput_prefix,imageList,params=None):
+def RunInferences(networkType,imageOutput_prefix,imageList,params=None):
  
-    targetShape=[40,40]
-
-
-    elif networkType=="resnet18":
-        from CoNNGaFit_NeuralNetwork_ResNet3d_18_parameterized import NeuralNetwork    
-    elif networkType=="unet18":
-        from CoNNGaFit_NeuralNetwork_Unet3d_18_parameterized import NeuralNetwork      
-    elif networkType=="resnet18_3dModel":
-        from CoNNGaFit_NeuralNetwork_ResNet3d_18_3dModel import NeuralNetwork
-        targetShape=[20,20,16]
-    elif networkType=="unet18_3dModel":
-        from CoNNGaFit_NeuralNetwork_Unet3d_18_3dModel import NeuralNetwork
-        targetShape=[20,20,16]
-
+    #####Set variables based on desired network type
+    
+    if params is None and networkType=='unet18': ####U-Net
+        nFilt0 = 6 #Number of filters
+        k0 = 9 #Initial kernel size
+        k1 = 3 #Kernel Size
+        nFC = 3000 #Nodes in FC layer
+        learning_rate= 0.0001 #lr used in training
+        weight_decay= 0.001 #wd used in training
+        epochs= 4000 #epochs used in training
+        params=[nFilt0,k0,k1,nFC]
         
-    if networkType=="resnet18" and params is not None:
+        targetShape=[40,40] #output dimensions
+        from CoNNGaFit_NeuralNetwork_Unet3d_18_parameterized import NeuralNetwork  
+        
+        paramSuffix = "lr"+str(learning_rate)+"_nFC"+str(nFC)+"_wd"+str(weight_decay)+"_nF"+str(nFilt0)+"_unetFullSpec"
+        modelOutputPath = 'TrainedNetworks\\MassFlux_Unet18_FullSpec'+paramSuffix+'_epoch'+str(epochs)+'.pt' #Address of desired model
+        
         model =NeuralNetwork(params[0],(params[1],params[1],params[1]),(params[2],params[2],params[2]),params[3]).to(device)
-    elif networkType=="unet18" and params is not None:
-        model =NeuralNetwork(params[0],(params[1],params[1],params[1]),(params[2],params[2],params[2]),params[3]).to(device)
-    else:
-        model = NeuralNetwork().to(device)
+
+
     model.load_state_dict(torch.load(modelOutputPath+".pt"))
     model.eval()
 
-#    imageList = "CoNNGaFitData/observations/inferencesToRun.csv"
-
-
-
-    #dirName = "fire2_md_withNoise"
-    #imageOutput_prefix = 'CoNNGaFitData/observations/'
     imageOutput_suffix = '_massFlux_FacePlot_'+networkType+"_TR_Comp_"
     plotOutput_suffix = '_massFlux_RadialPlot_'+networkType+"_TR_Comp_"
     compositeOutput_suffix = '_massFlux_CompositePlot_'+networkType+"_TR_Comp_"
 
 
-    try:
+    try: #Get normalization
         hf_model = h5py.File(modelOutputPath+".hdf5",'r')
         imageStats = np.array(hf_model['normalizationStats'])
         imageMean = imageStats[0]
         imageStdv = imageStats[1]
-    except:
+    except: #Default to per image normalization if training values not available for some reason
         dataset0 = CoNNGaFitImageInferenceDataset(annotations_file=imageList,
                                    root_dir = '.',
                                    transform = ToTensor())
@@ -232,32 +167,26 @@ def RunInferences(networkType,modelOutputPath, dirName,imageOutput_prefix,imageL
         imageStdv = data0[0].std()
         del loader0;del data0;del dataset0
 
-
-#dataset = datasets.ImageFolder("./CoNNGaFitData/observations/",transform = Compose([ToTensor() , Normalize(imageMean,imageStdv)]))
+    #Load spectral datacubes to run data on
     dataset = CoNNGaFitImageInferenceDataset(annotations_file=imageList,
                                    root_dir = '.',
                                    transform = Compose([ToTensor() , Normalize(imageMean,imageStdv)]))
     dataloader = torch.utils.data.DataLoader(dataset,batch_size=1,shuffle=False)
 
+    #Get names of images
     inferenceNames = LoadNames(imageList)
 
     i=1
-
-
+    #Make inference for each image loaded
     for inputs in dataloader:
         X=inputs.to(device)
         pred = model(X.float())
 
-        comparisonDir = comparisonRoot + inferenceNames[i]+ comparisonSuffix
-
-        vmax = np.max(np.abs(pred.cpu().detach().numpy()))
-        vmin = -vmax
-
         imageOutput = imageOutput_prefix + inferenceNames[i] + imageOutput_suffix 
         plotOutput = imageOutput_prefix + inferenceNames[i] + plotOutput_suffix 
 
-        MakeImage(pred.cpu().detach().numpy(),imageOutput,vmin,vmax,targetShape)
-        CreateBasicPlots(pred.cpu().detach().numpy(),plotOutput)
+        MakeImage(pred.cpu().detach().numpy(),imageOutput,targetShape) #Generate Projection Map
+        CreateBasicPlots(pred.cpu().detach().numpy(),plotOutput,targetShape) #Generate Radial Plots
         i+=1
 
     print("Done!")    
--- InferenceTemplate.py
+++ InferenceTemplate.py
@@ -1,48 +1,13 @@
-import torch
-from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor, Lambda, Normalize, Compose
-import sys
-
-import numpy as np
-from sklearn.preprocessing import StandardScaler    
-from matplotlib import pyplot as plt
-from matplotlib.colors import LogNorm
-import scipy.stats as stats
-
-import h5py
 from CoNNGaFit_UseModel import RunInferences
+#### User Input ############################  
 
-#### For Direct Use ####  
-
-epochs= 4000
-
-dirName = 'fire2_velocitySpace'
-
-
-
+####Network to use
 networkType = 'unet18'
+####Address of csv file with list of images to to apply model to
 imageList = "CoNNGaFitData/observations/inferencesToRun.csv"
-imageOutput = 'CoNNGaFitData/fire2_velocitySpace/i50/images/comparisons/output_example_'
-
-if networkType=='unet18':
-    nFilt0 = 6
-    k0 = 9
-    k1 = 3
-    nFC = 3000
-    learning_rate= 0.0001
-    weight_decay= 0.001
-    
-    
-outputSuffix = "lr"+str(learning_rate)+"_nFC"+str(nFC)+"_wd"+str(weight_decay)+"_nF"+str(nFilt0)+"_unetFullSpec"
-imageOutput = 'CoNNGaFitData\\'+dirName+'\\i50\\images\\comparisons\\'+subFolder+'\\massFlux'+outputSuffix+'_'
+####Directory to place outputs in
+imageOutput = "CoNNGaFitData/observations/output_example"
+############################################
 
 
-#modelPath = 'CoNNGaFitData\\'+dirName+'\\i'+str(inclination)+'\\trainedNetworks\\MassFlux_Unet18_FullSpec'+outputSuffix+'_epoch'+str(epochs)+'.pt'
-if subDir is None:
-    modelPath = 'CoNNGaFitData\\'+dirName+'\\i50\\trainedNetworks\\MassFlux_Unet18_FullSpec'+outputSuffix+'_epoch'+str(epochs)+'.pt'
-else:
-    modelPath = 'CoNNGaFitData\\'+dirName+'\\i50\\trainedNetworks\\'+subDir+'\\MassFlux_Unet18_FullSpec'+outputSuffix+'_epoch'+str(epochs)+'.pt'
-
-RunInferences(networkType , modelPath , dirName, imageOutput, imageList, [nFilt0,k0,k1,nFC])
+RunInferences(networkType , dirName, imageOutput, imageList)
